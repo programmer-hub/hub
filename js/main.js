2aa9233fe3e28cdbffd811dbb21d9a339261556f
Divay Prakash <divayprakash3@gmail.com>
2016-04-07 13:35:11 +0530

    Navbar fixed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m) {
+	i['GoogleAnalyticsObject']=r;
+	i[r]=i[r]||function() {
+	(i[r].q=i[r].q||[]).push(arguments)}, i[r].l=1*new Date();
+	a=s.createElement(o), m=s.getElementsByTagName(o)[0];
+	a.async=1;a.src=g; m.parentNode.insertBefore(a,m)
+})
+(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-76075660-1', 'auto');
 ga('send', 'pageview');
-$(document).ready(function(){
-$(window).scroll(function() {
-if ($(document).scrollTop() > $("#H").innerHeight() ) {
-$(".navbar-fixed-top").css("background-color", "#393939");
-} else {$(".navbar-fixed-top").css("background-color", "transparent");
-}
-});
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > $("#H").innerHeight()) {
+			$(".navbar-fixed-top").css("background-color", "#393939");
+		} 
+		else {
+			$(".navbar-fixed-top").css("background-color", "transparent");
+		}
+	});
 });
